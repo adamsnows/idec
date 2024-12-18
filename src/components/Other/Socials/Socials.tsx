0f1c2c217590039a487e10626cf507e84d76f1a8
Adam N <adaamangelow@gmail.com>
2024-12-18 11:17:26 -0300

    fix: correct links

--- src/components/Other/Socials/Socials.tsx
+++ src/components/Other/Socials/Socials.tsx
@@ -1,40 +1,64 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Socials = () => {
+  const [activeSection, setActiveSection] = useState("home");
+
+  const handleClick = (sectionId: string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+      setActiveSection(sectionId);
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      { threshold: 0.5 } // Adjust threshold as needed
+    );
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className="flex items-center xl:gap-x-5 gap-x-3 text-lg font-thin ">
-      <Link
-        target="_blank"
-        href={""}
+      <button
+        onClick={() => handleClick("about")}
         className="hover:text-accent transition-all duration-300"
-        aria-label={"linkedin"}
+        aria-label={"buttonedin"}
       >
         SOBRE
-      </Link>
-      <Link
-        target="_blank"
-        href={""}
+      </button>
+      <button
+        onClick={() => handleClick("formation")}
         className="hover:text-accent transition-all duration-300"
         aria-label={"instagram"}
       >
         FORMAÇÕES
-      </Link>
-      <Link
-        target="_blank"
-        href=""
+      </button>
+      <button
+        onClick={() => handleClick("unity")}
         className="hover:text-accent transition-all duration-300"
         aria-label={"whatsapp"}
       >
         UNIDADES
-      </Link>
-      <Link
-        target="_blank"
-        href="mailto:"
+      </button>
+      <button
+        onClick={() => handleClick("contact")}
         className="hover:text-accent transition-all duration-300"
         aria-label={"email"}
       >
         CONTATO
-      </Link>
+      </button>
     </div>
   );
 };
